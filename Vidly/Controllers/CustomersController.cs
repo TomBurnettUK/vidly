60390a1936d716ddaa3e11fcafa8d51526ac7e0d
Tom Burnett <t_burnett@outlook.com>
2017-07-04 12:11:35 +0100

    Add Name to MembershipType

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,34 +10,30 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-        public List<Customer> Customers { get; set; }
+        private ApplicationDbContext context;
 
         public CustomersController()
         {
-            this.Customers = new List<Customer>
-            {
-                new Customer() { Id = 1, Name = "Fred" },
-                new Customer() { Id = 2, Name = "Mary" },
-                new Customer() { Id = 3, Name = "Helen" },
-                new Customer() { Id = 4, Name = "Dave" },
-                new Customer() { Id = 5, Name = "Steve" },
-                new Customer() { Id = 6, Name = "Karen" },
-            };
+            this.context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.context.Dispose();
         }
 
         // GET: Customer
         public ActionResult Index()
         {
-            var viewModel = new CustomerListViewModel() { Customers = this.Customers };
-
-            return View(viewModel);
+            var customers = this.context.Customers.Include("MembershipType").ToList();
+            return View(customers);
         }
 
         [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var model = this.Customers.Find(customer => customer.Id == id);
-            return View(model);
+            var customer = this.context.Customers.SingleOrDefault(c => c.Id == id);
+            return View(customer);
         }
     }
 }
